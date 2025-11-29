e084019aef36e49992a1d3207e8a8ffe1b66fec8
RajanPatel0 <rajanpatel.ias@gmail.com>
2025-11-29 22:52:32 +0530

    checking

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <h1 class="text-green-500 font-bold underline">Hello world! Introducing INVOLV</h1>
+    <h1 class="text-green-500 font-bold underline">Hello world! Introducing INVOLV!!</h1>
   )
 }
 
