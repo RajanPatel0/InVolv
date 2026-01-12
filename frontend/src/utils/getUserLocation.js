e77d7df45e182314a7756b30225dd4307521856a
RajanPatel0 <rajanpatel.ias@gmail.com>
2026-01-12 23:49:44 +0530

    search hero done

---
+++ frontend/src/utils/getUserLocation.js
@@ -0,0 +1,17 @@
+export const getUserLocation = () =>
+  new Promise((resolve, reject) => {
+    if (!navigator.geolocation) {
+      reject("Geolocation not supported");
+      return;
+    }
+
+    navigator.geolocation.getCurrentPosition(
+      (pos) =>
+        resolve({
+          lat: pos.coords.latitude,
+          lng: pos.coords.longitude,
+        }),
+      (err) => reject(err),
+      { enableHighAccuracy: true, timeout: 10000 }
+    );
+  });
